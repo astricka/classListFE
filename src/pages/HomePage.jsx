f3425ee52f70af7ef116252f457216649a1b03ac
Algirdas Stricka <algirdasstricka@yahoo.com>
2021-12-15 11:25:09 +0200

    deleted home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import Listing from '../components/Listing/Listing';
 function HomePage() {
     return(
         <main>
-            <h1>Home page</h1>
             <Listing />
         </main>
     );
